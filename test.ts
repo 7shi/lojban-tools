805a1f60ecdffb30532d255220d1f2ab4795732b
7shi <7shi@live.jp>
2022-10-28 19:02:46 +0900

    split `similarity`

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-function similarity(g: string, w: string) {
-    if (!w) return [0, 0];
+function similarity2a(g: string, w: string): number {
+    if (!g || !w) return 0;
     const memo = Array(g.length).fill([]).map(() => Array(w.length));
     function f(gp: number, wp: number) {
         if (gp == g.length || wp == w.length) return 0;
@@ -9,23 +9,35 @@ function similarity(g: string, w: string) {
         const p = g.indexOf(w[wp], gp);
         return memo[gp][wp] = p < 0 ? sc : Math.max(sc, f(p + 1, wp + 1) + 1);
     }
-    const ret = f(0, 0);
-    if (ret >= 3) return [ret, ret / w.length];
+    return f(0, 0);
+}
+
+function similarity2b(g: string, w: string): number {
+    if (!g || !w) return 0;
     for (let i = 0; i < w.length - 1; i++) {
         let j = -1;
         while ((j = g.indexOf(w[i], j + 1)) >= 0) {
             if ((g[j + 1] == w[i + 1] || (i < w.length - 2 && g[j + 2] == w[i + 2])))
-                return [2, 2 / w.length];
+                return 2;
         }
     }
-    return [0, 0];
+    return 0;
 }
 
-function gismu(weights: number[], jbo: string, words: string[]) {
-    while (words.length < 6) words.push("");
-    const scores = words.map(w => similarity(jbo, w));
-    let sum = scores.map((sc, i) => sc[1] * weights[i]).reduce((x, y) => x + y);
-    return { sum: sum, scores: scores.map(sc => sc[0]) };
+function similarity(g: string, w: string) {
+    let s = similarity2a(g, w);
+    return s < 3 ? similarity2b(g, w) : s;
+}
+
+function gismu(weights: number[], g: string, words: string[]) {
+    let score = 0, sims: number[] = [];
+    for (let i = 0; i < 6; i++) {
+        const w = words[i];
+        let s = similarity(g, w);
+        score += s ? s / w.length * weights[i] : 0;
+        sims.push(s);
+    }
+    return { score: score, sims: sims };
 }
 
 const weights = {
@@ -40,8 +52,8 @@ const weights = {
 let all = 0, ng = 0;
 function test(word, data) {
     const sc = gismu(weights["????"], word, data.words);
-    const score = Math.floor(sc.sum * 10000) / 100;
-    const sc1 = score.toFixed(2), sc2 = sc.scores.join(" ");
+    const score = Math.floor(sc.score * 10000) / 100;
+    const sc1 = score.toFixed(2), sc2 = sc.sims.join(" ");
     const ok1 = sc1 == data.score, ok2 = sc2 == data.scores;
     if (!ok1 || !ok2) {
         let diff = (score - parseFloat(data.score)).toFixed(2);
