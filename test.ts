cf35058b2ef757f2ee0027bd370f8cd688b4628c
7shi <7shi@live.jp>
2022-10-28 23:33:08 +0900

    add unused lines and comment them out

--- test.ts
+++ test.ts
@@ -38,7 +38,30 @@ const weights = {
     "????": [0.330, 0.180, 0.160, 0.120, 0.120, 0.070], // sum: 0.98
 };
 
-function score(weights: number[], g: string, words: string[]) {
+interface FinPrim { score: string, sims: string, words: string[] }
+const finprims: { [index: string]: FinPrim } =
+    JSON.parse(Deno.readTextFileSync("finprims.json"));
+
+// let all = 0, ng = 0;
+// for (const [g, data] of Object.entries(finprims)) {
+//     const sims = data.words.map(w => similarity(g, w));
+//     if (sims.join(" ") != data.sims) ng++;
+//     all++;
+// }
+// console.log("NG:", ng, "/", all);
+
+// const data = finprims["gismu"];
+// console.log(data);
+// for (const [key, ws] of Object.entries(weights)) {
+//     let score = 0;
+//     for (let i = 0; i < 6; i++) {
+//         const word = data.words[i];
+//         score += similarity("gismu", word) / word.length * ws[i];
+//     }
+//     console.log(key, (score * 100).toFixed(2));
+// }
+
+function score(g: string, words: string[], weights: number[]) {
     let score = 0, sims: number[] = [];
     for (let i = 0; i < 6; i++) {
         const w = words[i];
@@ -49,26 +72,24 @@ function score(weights: number[], g: string, words: string[]) {
     return { score: score, sims: sims };
 }
 
-let all = 0, ng = 0;
-function test(word, data) {
-    const ss = score(weights["????"], word, data.words);
+const ws = weights["????"];
+// const ws = [0.330, 0.180, 0.160, 0.120, 0.120, 0.070]; // sum: 0.98
+let all = 0, ng = 0, diffs = {};
+for (const [g, data] of Object.entries(finprims)) {
+    const ss = score(g, data.words, ws);
     const sc = Math.floor(ss.score * 10000) / 100;
     const sc1 = sc.toFixed(2), sc2 = ss.sims.join(" ");
     const ok1 = sc1 == data.score, ok2 = sc2 == data.sims;
     if (!ok1 || !ok2) {
         let diff = (sc - parseFloat(data.score)).toFixed(2);
         if (!diff.startsWith("-")) diff = "+" + diff;
-        console.log(word,
+        if (diff in diffs) diffs[diff]++; else diffs[diff] = 0;
+        console.log(g,
             ok1 ? "[OK]" : "[" + diff + "]", sc1, "exp =", data.score,
-            ok2 ? "[OK]" : "[NG]", sc2, "exp =", data.sims,
+            //ok2 ? "[OK]" : "[NG]", sc2, "exp =", data.sims,
             "|", data.words.join(" "));
         ng++;
     }
     all++;
 }
-
-interface FinPrim { score: string, sims: string, words: string[] }
-const finprims: { [index: string]: FinPrim } =
-    JSON.parse(Deno.readTextFileSync("finprims.json"));
-for (const [word, data] of Object.entries(finprims)) test(word, data);
-console.log("NG:", ng, "/", all);
+console.log("NG:", ng, "/", all, "diffs:", diffs);
