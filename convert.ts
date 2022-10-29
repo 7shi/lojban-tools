dd79c424e286598a3aae9142f3d3c8d707450c9c
7shi <7shi@live.jp>
2022-10-30 03:43:40 +0900

    if spelled differently, prefer headings

--- convert.ts
+++ convert.ts
@@ -5,22 +5,32 @@ if (Deno.args.length != 2) {
 
 const finprims = Deno.readTextFileSync(Deno.args[0]);
 const data = {};
-let words: string[] = [];
+let word = "", words: string[] = [], reserved = {};
 let count = 0;
 for (const line of finprims.split("\r\n")) {
     const d = line.substring(14);
-    if (!words.length && d.match(/^[a-z]+( [a-z]+){3,5}$/)) {
+    const m1 = d.match(/^([a-z]+) {5}\d\d\.\d\d {5}/);
+    const m2 = d.match(/^[a-z]+( [a-z]*){3,5}$/);
+    const m3 = d.match(/([a-z?]+)  (\d\d\.\d\d) (\d( \d){5})/);
+    if (m1) {
+        if (word && reserved["score"]) data[word] = reserved;
+        word = m1[1];
+        words = [];
+        reserved = {};
+    } else if (m2 && (!words.length || reserved["score"])) {
         words = d.split(" ");
         while (words.length < 6) words.push("");
-    } else if (words.length) {
-        const m = d.match(/([a-z?]+)  (\d\d\.\d\d) (\d( \d){5})/);
-        if (m) {
-            const sims = m[3].split(" ").map(s => parseInt(s));
-            data[m[1]] = { score: m[2], sims: sims, words: words };
-            words = [];
-        } else if (line.substring(0, 14).trim()) {
-            words = [];
-        }
+        reserved = {};
+    } else if (m3 && words.length) {
+        const sims = m3[3].split(" ").map(s => parseInt(s));
+        const r = { score: m3[2], sims: sims, words: words };
+        if (m3[1] == word) {
+            data[word] = r;
+            word = "";
+        } else reserved = r;
     }
 }
+if (word && reserved["score"]) data[word] = reserved;
+
 Deno.writeTextFileSync(Deno.args[1], JSON.stringify(data));
+//Deno.writeTextFileSync(Deno.args[1] + ".txt", Object.keys(data).join("\n") + "\n");
