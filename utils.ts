a4e08bdfecf0f2ee95255c064aea93ee1e780394
7shi <7shi@live.jp>
2022-11-04 02:42:28 +0900

    add `splitLines`, `readLines`, `writeLines`

--- utils.ts
+++ utils.ts
@@ -17,3 +17,28 @@ export function writeJSON(path: string, data: {}) {
     }
     Deno.writeTextFileSync(path, text);
 }
+
+export function* splitLines(text: string) {
+    let line = "", prev = "";
+    for (const ch of text) {
+        if (ch == '\r') {
+            yield line;
+            line = "";
+        } else if (ch == '\n') {
+            if (prev != '\r') yield line;
+            line = "";
+        } else {
+            line += ch;
+        }
+        prev = ch;
+    }
+    if (line.length) yield line;
+}
+
+export function readLines(fileName: string) {
+    return Array.from(splitLines(Deno.readTextFileSync(fileName)));
+}
+
+export function writeLines(fileName: string, lines: string[]) {
+    Deno.writeTextFileSync(fileName, lines.join("\n") + "\n");
+}
